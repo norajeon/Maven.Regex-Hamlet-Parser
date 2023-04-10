1571eaa37b4c2da574f71d4a1784be5152c979c0
nora <sxj042@gmail.com>
2023-04-10 18:40:50 -0400

    ok

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -36,4 +38,89 @@ public class HamletParser {
         return hamletData;
     }
 
+    public void changeHamletToLeon() {
+        Pattern pattern = Pattern.compile("Hamlet");
+        Matcher matcher = pattern.matcher(hamletData);
+        String replacementText = "Leon";
+        StringBuffer sb = new StringBuffer();
+
+        while(matcher.find()) {
+
+            matcher.appendReplacement(sb, replacementText);
+
+        }
+        matcher.appendTail(sb);
+        hamletData = sb.toString();
+
+
+
+        pattern = Pattern.compile("HAMLET");
+        matcher = pattern.matcher(hamletData);
+        String replacementText2 = "LEON";
+
+
+        sb = new StringBuffer();
+
+        while(matcher.find()) {
+
+            matcher.appendReplacement(sb, replacementText2);
+
+        }
+        matcher.appendTail(sb);
+        hamletData = sb.toString();
+
+    }
+
+    public void changeHamletToTariq() {
+        Pattern pattern = Pattern.compile("Horatio");
+        Matcher matcher = pattern.matcher(hamletData);
+        String replacementText = "Tariq";
+        StringBuffer sb = new StringBuffer();
+
+        while(matcher.find()) {
+
+            matcher.appendReplacement(sb, replacementText);
+
+        }
+        matcher.appendTail(sb);
+        hamletData = sb.toString();
+
+
+
+        pattern = Pattern.compile("HORATIO");
+        matcher = pattern.matcher(hamletData);
+        String replacementText2 = "TARIQ";
+
+
+        sb = new StringBuffer();
+
+        while(matcher.find()) {
+
+            matcher.appendReplacement(sb, replacementText2);
+
+        }
+        matcher.appendTail(sb);
+        hamletData = sb.toString();
+
+    }
+
+    public int findHoratio() {
+        Pattern pattern = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletData);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
+    }
+
+    public int findHamlet() {
+        Pattern pattern = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletData);
+        int count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+        return count;
+    }
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,9 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
@@ -15,17 +19,52 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        hamletParser.changeHamletToLeon();
+        Pattern pattern = Pattern.compile("Hamlet", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletParser.getHamletData());
+
+        String ham = hamletParser.getHamletData();
+        Assert.assertFalse(matcher.find());
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        hamletParser.changeHamletToTariq();
+        Pattern pattern = Pattern.compile("Horatio", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(hamletParser.getHamletData());
+
+        Assert.assertFalse(matcher.find());
     }
 
     @Test
     public void testFindHoratio() {
+        int actual = hamletParser.findHoratio();
+        int expected = 158;
+
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
     public void testFindHamlet() {
+        int actual = hamletParser.findHamlet();
+        int expected = 472;
+
+        Assert.assertEquals(actual, expected);
     }
+
+//    @Test
+//    public void testFindLeon() {
+//        Pattern pattern = Pattern.compile("Leon", Pattern.CASE_INSENSITIVE);
+//        Matcher matcher = pattern.matcher(hamletText);
+//        Assert.assertTrue(matcher.find());
+//
+//    }
+//
+//    @Test
+//    public void testFindTariq() {
+//        Pattern pattern = Pattern.compile("Tariq", Pattern.CASE_INSENSITIVE);
+//        Matcher matcher = pattern.matcher(hamletText);
+//        Assert.assertTrue(matcher.find());
+//
+//    }
 }
